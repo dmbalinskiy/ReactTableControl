d24bd8e7e7dd3a14826d007f5e8ed05d7d5f99e3
Dmytro Balinskyi <dimaba@xmpie.com>
2024-03-10 05:09:08 +0200

    ongoing development - tab panel is used for two tables, table refactoring

--- table-control/src/App.js
+++ table-control/src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 // Filename - App.js
 import './App.css';
 import Table from './components/table/table'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import TabPanel from './tabPanel/tabPanel';
 
 function App() {
-    
-    
     return (
-        <Table />
+        <StyledEngineProvider injectFirst>
+            <TabPanel />
+        </StyledEngineProvider>
+        // <Table />
     )
 }
 
--- table-control/src/components/table/table.js
+++ table-control/src/components/table/table.js
@@ -6,9 +6,9 @@ import './table.css'
 const maxColumnLength = 64;
 const maxRowLength = 2048;
 
-function Table({rows, }) {
-
-    const [tableData, setTableData] = useState(tableDataInitial);
+function Table() {
+    //let tableDataInitial = addVirtualItems( getInitialData());
+    const [tableData, setTableData] = useState(addVirtualItems( getInitialData()));
     let selection = useRef(null);
 
     useEffect(() => {
@@ -27,7 +27,7 @@ function Table({rows, }) {
         let row = newTd.rows.find(row => row.idx === cellData.rowIdx);
         newTd.rows = [
             ...newTd.rows.slice(0, row.idx + (addBefore ? 0 : 1)),
-            getNewRow(newTd, ++currentId, row),
+            getNewRow(newTd, row),
             ...newTd.rows.slice(row.idx + (addBefore ? 0 : 1))
         ]
         adjustRowIndexes(newTd);
@@ -82,6 +82,177 @@ function Table({rows, }) {
         //console.log(cell);
     }
 
+    function adjustRowIndexes(tableData){
+        for(let i = 0; i < tableData.rows.length; i++){
+            let row = tableData.rows[i];
+            row.idx = i;
+            for(let j = 0; j < row.cells.length; j++){
+                let cell = row.cells[j];
+                adjustCellIndex(cell, j, i, tableData.rows.length, row.cells.length, false, row.id);
+            }
+        }
+    }
+    
+    function getNewRow(tableData, previousRow, useId = true){
+        let cells = previousRow.cells.map(
+            (val, idx) =>{
+                return {
+                    id : val.id,
+                    text: ``,
+                    isHeader: idx === 0,
+                }
+            });
+        let row = {
+            idx : -1,
+            cells : cells,
+            cellIds : new Set(cells.map(c => c.id))
+        }
+        if(useId){
+            ApplyIdToRow(tableData, row);
+        }
+        
+        return row;
+    }
+    function addColumnToRow(row, cell, isHeaderCell = false, addBefore = false){
+        let newCell = {text: ``, isHeader : isHeaderCell};
+        row.cells = [
+            ...row.cells.slice(0, cell.idx + (addBefore ? 0 : 1)),
+            newCell,
+            ...row.cells.slice(cell.idx + (addBefore ? 0 : 1))
+        ]
+        ApplyIdToCell(row, newCell);
+        return row;
+    }
+    function deleteColumnFromRow(row, cell){
+        row.cells.splice(cell.idx, 1);
+        row.cellIds.delete(cell.id);
+        return row;
+    }
+    function adjustCellIndex(cell, cellIdx, rowIdx, rowCnt, colCnt, isVirtual, rowId){
+        cell.idx = cellIdx;
+        cell.rowIdx = rowIdx;
+        cell.rowCnt = rowCnt;
+        cell.colCnt = colCnt;
+        cell.isVirtual = isVirtual;
+    }
+    function addVirtualItems(tableData){
+        let rows = tableData.rows;
+        let lastRow = rows[rows.length - 1];
+        const firstCell = lastRow.cells[0];
+        if(firstCell.isVirtual){
+            return;
+        }
+    
+        const firstRow = rows[0];
+        const lastCell = firstRow.cells[firstRow.cells.length - 1];
+        if(lastCell.isVirtual){
+            return;
+        }
+    
+        rows = rows.map(row => {
+            return addColumnToRow(row, row.cells[row.cells.length - 1]);
+        });
+    
+        var row = getNewRow(tableData, rows[rows.length - 1], false);
+        row.idx = rows.length;
+        rows = [...rows, row];
+    
+        //adjust indexes - for last column
+        for(let i = 0; i < rows.length; i++){
+            let row = rows[i];
+            let cell = row.cells[row.cells.length - 1];
+            adjustCellIndex(cell, row.cells.length - 1, i, rows.length, row.cells.length, true, row.id);
+        }
+    
+        //adjust indexes - for last row
+        lastRow = rows[rows.length - 1];
+        for(let i = 0; i < row.cells.length; i++){
+            let cell = row.cells[i];
+            adjustCellIndex(cell, i, row.idx, rows.length, row.cells.length, true, row.id);
+        }
+    
+        tableData.rows = rows;
+        return tableData;
+    }
+    
+    function removeVirtualItems(tableData){
+        tableData = removeVirtualRow(tableData);
+        tableData = removeVirtualColumn(tableData);
+        return tableData;
+    }
+    function removeVirtualRow(tableData){
+        const rows = tableData.rows;
+        const lastRow = rows[rows.length - 1];
+        const firstCell = lastRow.cells[0];
+        if(firstCell.isVirtual){
+            rows.splice(rows.length - 1, 1);
+        }
+        tableData.rows = rows;
+        return tableData;
+    }
+    function removeVirtualColumn(tableData){
+        let rows = tableData.rows;
+        const firstRow = rows[0];
+        const lastCell = firstRow.cells[firstRow.cells.length - 1];
+        if(lastCell.isVirtual){
+            rows = rows.map(row => {
+                return deleteColumnFromRow(row, row.cells[row.cells.length - 1]);
+            });
+        }
+        tableData.rows = rows;
+        return tableData;
+    }
+    
+    function ApplyIdToCell(row, cell){
+        let id = maxColumnLength * 2;
+        for(let i = 0; i < maxColumnLength * 2; i++){
+            if(!row.cellIds.has(i)){
+                id = i;
+                break;
+            }
+        }
+        cell.id = id;
+        row.cellIds.add(id);
+    }
+    
+    function ApplyIdToRow(tableData, row){
+        let id = maxRowLength*2;
+        for(let i = 0; i < maxRowLength * 2; i++){
+            if(!tableData.rowIds.has(i)){
+                id = i;
+                break;
+            }
+        }
+        row.id = id;
+        tableData.rowIds.add(id);
+    }
+    
+    function getInitialData() {
+        let tableData = 
+        {
+            rowIds: new Set(),
+            rows:
+            [
+                {id: 0, idx:0, cells: [{ id: 0, idx: 0, rowIdx:0, text: "", isHeader: 'true', isVirtual : false}, { id: 1, idx:1, rowIdx:0, text: "h2", isHeader: 'true', isVirtual : false}], cellIds : new Set() },
+                {id: 1, idx:1, cells: [{ id: 0, idx: 0, rowIdx:1, text: "val 1", isHeader: 'true', isVirtual : false}, { id: 1, idx:1, rowIdx:0, text: "val 2", isVirtual : false}], cellIds : new Set() },
+            ]
+        }
+        for(let row of tableData.rows){
+            ApplyIdToRow(tableData, row);
+            for(let id of row.cells.map(c => c.id)){
+                row.cellIds.add(id);
+            }
+        }
+        return tableData;
+    }
+    
+    function copyTableData(tableData){
+        return {
+            rowIds : tableData.rowIds,
+            rows: tableData.rows,
+        }
+    }
+    
     return (
         <div>
             <table>
@@ -104,178 +275,5 @@ function Table({rows, }) {
         </div>
     )
 }
-
-function adjustRowIndexes(tableData){
-    for(let i = 0; i < tableData.rows.length; i++){
-        let row = tableData.rows[i];
-        row.idx = i;
-        for(let j = 0; j < row.cells.length; j++){
-            let cell = row.cells[j];
-            adjustCellIndex(cell, j, i, tableData.rows.length, row.cells.length, false, row.id);
-        }
-    }
-}
-
-function getNewRow(tableData, id, previousRow, useId = true){
-    let cells = previousRow.cells.map(
-        (val, idx) =>{
-            return {
-                id : val.id,
-                text: ``,
-                isHeader: idx === 0,
-            }
-        });
-    let row = {
-        idx : -1,
-        cells : cells,
-        cellIds : new Set(cells.map(c => c.id))
-    }
-    if(useId){
-        ApplyIdToRow(tableData, row);
-    }
-    
-    return row;
-}
-function addColumnToRow(row, cell, isHeaderCell = false, addBefore = false){
-    let newCell = {text: ``, isHeader : isHeaderCell};
-    row.cells = [
-        ...row.cells.slice(0, cell.idx + (addBefore ? 0 : 1)),
-        newCell,
-        ...row.cells.slice(cell.idx + (addBefore ? 0 : 1))
-    ]
-    ApplyIdToCell(row, newCell);
-    return row;
-}
-function deleteColumnFromRow(row, cell){
-    row.cells.splice(cell.idx, 1);
-    row.cellIds.delete(cell.id);
-    return row;
-}
-function adjustCellIndex(cell, cellIdx, rowIdx, rowCnt, colCnt, isVirtual, rowId){
-    cell.idx = cellIdx;
-    cell.rowIdx = rowIdx;
-    cell.rowCnt = rowCnt;
-    cell.colCnt = colCnt;
-    cell.isVirtual = isVirtual;
-}
-function addVirtualItems(tableData){
-    let rows = tableData.rows;
-    let lastRow = rows[rows.length - 1];
-    const firstCell = lastRow.cells[0];
-    if(firstCell.isVirtual){
-        return;
-    }
-
-    const firstRow = rows[0];
-    const lastCell = firstRow.cells[firstRow.cells.length - 1];
-    if(lastCell.isVirtual){
-        return;
-    }
-
-    rows = rows.map(row => {
-        return addColumnToRow(row, row.cells[row.cells.length - 1]);
-    });
-
-    var row = getNewRow(tableData, ++currentId, rows[rows.length - 1], false);
-    row.idx = rows.length;
-    rows = [...rows, row];
-
-    //adjust indexes - for last column
-    for(let i = 0; i < rows.length; i++){
-        let row = rows[i];
-        let cell = row.cells[row.cells.length - 1];
-        adjustCellIndex(cell, row.cells.length - 1, i, rows.length, row.cells.length, true, row.id);
-    }
-
-    //adjust indexes - for last row
-    lastRow = rows[rows.length - 1];
-    for(let i = 0; i < row.cells.length; i++){
-        let cell = row.cells[i];
-        adjustCellIndex(cell, i, row.idx, rows.length, row.cells.length, true, row.id);
-    }
-
-    tableData.rows = rows;
-    return tableData;
-}
-
-function removeVirtualItems(tableData){
-    tableData = removeVirtualRow(tableData);
-    tableData = removeVirtualColumn(tableData);
-    return tableData;
-}
-function removeVirtualRow(tableData){
-    const rows = tableData.rows;
-    const lastRow = rows[rows.length - 1];
-    const firstCell = lastRow.cells[0];
-    if(firstCell.isVirtual){
-        rows.splice(rows.length - 1, 1);
-    }
-    tableData.rows = rows;
-    return tableData;
-}
-function removeVirtualColumn(tableData){
-    let rows = tableData.rows;
-    const firstRow = rows[0];
-    const lastCell = firstRow.cells[firstRow.cells.length - 1];
-    if(lastCell.isVirtual){
-        rows = rows.map(row => {
-            return deleteColumnFromRow(row, row.cells[row.cells.length - 1]);
-        });
-    }
-    tableData.rows = rows;
-    return tableData;
-}
-
-function ApplyIdToCell(row, cell){
-    let id = maxColumnLength * 2;
-    for(let i = 0; i < maxColumnLength * 2; i++){
-        if(!row.cellIds.has(i)){
-            id = i;
-            break;
-        }
-    }
-    cell.id = id;
-    row.cellIds.add(id);
-}
-
-function ApplyIdToRow(tableData, row){
-    let id = maxRowLength*2;
-    for(let i = 0; i < maxRowLength * 2; i++){
-        if(!tableData.rowIds.has(i)){
-            id = i;
-            break;
-        }
-    }
-    row.id = id;
-    tableData.rowIds.add(id);
-}
-
-let currentId = 2;
-function getInitialData() {
-    let tableData = 
-    {
-        rowIds: new Set(),
-        rows:
-        [
-            {id: 0, idx:0, cells: [{ id: 0, idx: 0, rowIdx:0, text: "", isHeader: 'true', isVirtual : false}, { id: 1, idx:1, rowIdx:0, text: "h2", isHeader: 'true', isVirtual : false}], cellIds : new Set() },
-            {id: 1, idx:1, cells: [{ id: 0, idx: 0, rowIdx:1, text: "val 1", isHeader: 'true', isVirtual : false}, { id: 1, idx:1, rowIdx:0, text: "val 2", isVirtual : false}], cellIds : new Set() },
-        ]
-    }
-    for(let row of tableData.rows){
-        ApplyIdToRow(tableData, row);
-        for(let id of row.cells.map(c => c.id)){
-            row.cellIds.add(id);
-        }
-    }
-    return tableData;
-}
-
-function copyTableData(tableData){
-    return {
-        rowIds : tableData.rowIds,
-        rows: tableData.rows,
-    }
-}
-let tableDataInitial = addVirtualItems( getInitialData());
 
 export default Table;
