4c348408310921426fae0f57bf3f3ec8665c438e
Dmytro Balinskyi <dimaba@xmpie.com>
2024-03-10 04:24:24 +0200

    material-ui added

--- table-control/src/index.js
+++ table-control/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
